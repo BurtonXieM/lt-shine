d3ec60d4d180444482849ef9623df2170744adbd
xiemeng <xiemeng@99114.com>
2016-11-01 20:12:27 +0800

    依赖

--- src/main/java/com/cn/lt/member/service/serviceImpl/MemberServiceImpl.java
+++ src/main/java/com/cn/lt/member/service/serviceImpl/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package com.cn.lt.member.service.serviceImpl;
 import com.cn.lt.member.dao.MemberMapper;
 import com.cn.lt.member.entity.Member;
 import com.cn.lt.member.service.MemberService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 /**
  * Created by XieM on 2016/10/29.
  */
+@Service("memberService")
 public class MemberServiceImpl implements MemberService {
 
     @Resource
